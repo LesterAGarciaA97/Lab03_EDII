6ea263c371c969bed7681af19416af16a2d8b474
Lester Andrés García Aquino <lagarcia2015url@gmail.com>
2020-09-27 19:46:26 -0600

    Se agregó consola de pruebas

---
+++ Solución/Lab03_EDII/ConsolaDePruebas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsolaDePruebas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
