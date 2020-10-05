29b3f2ffe243f99109c1d40e31a2d5e3fc93fade
HectorZetino <h.rzc.zetino@hotmail.es>
2020-10-05 11:48:11 -0600

    Agregando nodo arbol Huffman

--- Solución/Lab03_EDII/BibliotecaDeClases/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace BibliotecaDeClases
-{
-    public class Class1
-    {
-    }
-}
---
+++ Solución/Lab03_EDII/BibliotecaDeClases/Huffman/Huffman.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BibliotecaDeClases.Huffman
+{
+    public class Node : IComparable {
+        public byte byt;
+        public int freq;
+        public Node Left = null;
+        public Node Right = null;
+        public Node(byte _byt, int _freq, Node _Left , Node _Right)
+        {
+            this.byt = _byt;
+            this.freq = _freq;
+            this.Left = _Left;
+            this.Right = _Right;
+        }
+        Node(byte _byt, int _freq) {
+            this.byt = _byt;
+            this.freq = _freq;
+        }
+
+        public int CompareTo(object obj)
+        {
+            var obj2 = (Node)obj;
+            return freq.CompareTo(obj2.freq);
+        }
+    }
+    class Huffman
+    {
+    }
+}
