74157adbf20cf4b32b6ec9b3f25aa8d5943de2d7
HectorZetino <h.rzc.zetino@hotmail.es>
2020-10-08 18:18:06 -0600

    Agregando algoritmo Huffman Completo

---
+++ Solución/Lab03_EDII/BibliotecaDeClases/Huffman/CompressionsCollections.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BibliotecaDeClases.Huffman
+{
+
+    public class CompressionsCollection
+    {
+        public string originalName { get; set; }
+        public string path { get; set; }
+        public double compressionReason { get; set; } // razon de compresion 
+        public double compressionFactor { get; set; }  //factor de compresion 
+        public string reduction { get; set; }
+
+        public CompressionsCollection(string name, string path, double cr, double cf, string redPercentage)
+        {
+            originalName = name;
+            this.path = path;
+            compressionReason = cr;
+            compressionFactor = cf;
+            reduction = redPercentage;
+        }
+    }
+}
